2335c495667dca55b0dd2135240e77058a6d454c
Mauro <maurodefalco96@gmail.com>
2024-12-17 00:17:06 +0100

    weight

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+ import { ref } from 'vue'
 
+ const weight = ref(0)
+ const currentWeight = ref(0)
+ const allWeights = ref([])
+
+ const submitForm = (e: Event) => {
+   e.preventDefault()
+   let newWeight = {
+     weight: weight.value,
+     date: new Date()
+   }
+   allWeights.value.push(newWeight)
+   currentWeight.value = newWeight.weight
+ }
 </script>
 
 <template>
-  <h1 class="m-4 text-4xl">Hello World!</h1>
+  <h1 class="m-4 text-4xl">{{ currentWeight }}</h1>
+
+  <form class="flex items-center" @submit="submitForm">
+    <label for="weight">Weight</label>
+    <input class="border-2 border-black rounded" type="number" id="weight" v-model="weight">
+    <button class="bg-blue-500 text-white px-4 py-2 rounded" type="submit">Submit</button>
+  </form>
+
+
+  <ul v-if="allWeights.length > 0" class="m-4 list-disc">
+    <div class="flex justify-between">
+     <h2 class="text-xl">Weights History</h2>
+     <h5 class="text-gray-500 italic px-4 py-2 rounded">Latest 7 Days</h5>
+    </div>
+    <li v-for="w in allWeights">{{ w.weight }}</li>
+  </ul>
+
 </template>
 
 <style scoped>
