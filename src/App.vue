8722e291e229de7276b0393c3b41e646996fb761
Mauro <maurodefalco96@gmail.com>
2024-12-17 00:45:20 +0100

    history tabs

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,26 @@
 </script>
 
 <template>
-  <h1 class="m-4 text-4xl">{{ currentWeight }}</h1>
+  <h1 class="my-4 text-4xl mx-auto text-center">
+    {{ currentWeight }} <span class="text-sm">/ kg</span>
+  </h1>
 
-  <form class="flex items-center" @submit="submitForm">
-    <label for="weight">Weight</label>
-    <input class="border-2 border-black rounded" type="number" id="weight" v-model="weight">
+  <form class="justify-center flex gap-2" @submit="submitForm">
+    
+    <input step="0.1" class="border-2 border-black rounded" type="number" id="weight" v-model="weight">
     <button class="bg-blue-500 text-white px-4 py-2 rounded" type="submit">Submit</button>
   </form>
 
 
-  <ul v-if="allWeights.length > 0" class="m-4 list-disc">
+  <div v-if="allWeights.length > 0" class="m-4">
     <div class="flex justify-between">
      <h2 class="text-xl">Weights History</h2>
      <h5 class="text-gray-500 italic px-4 py-2 rounded">Latest 7 Days</h5>
     </div>
-    <li v-for="w in allWeights">{{ w.weight }}</li>
-  </ul>
+    <ul>
+      <li class="bg-slate-200 even:bg-slate-300 py-1 px-2" v-for="w in allWeights">{{ w.weight }}</li>
+    </ul>
+  </div>
 
 </template>
 
