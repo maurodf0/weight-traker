51e2d145a7bd7225727f5b67b9559d99d5b83593
Mauro <maurodefalco96@gmail.com>
2024-12-17 23:33:54 +0100

    LATEST

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ const optionsIT = { weekday:"long", year: "numeric", month: "long", day: "numeri
   <div v-if="allWeights.length > 0" class="m-4">
     <div class="flex justify-between">
      <h2 class="text-xl">Weights History</h2>
-     <h5 class="text-gray-500 italic px-4 py-2 rounded">Latest 7 Days</h5>
+     <h5 class="text-gray-500 italic px-4 py-2 rounded">Latests</h5>
     </div>
     <ul>
       <li class=" flex justify-between bg-slate-200 even:bg-slate-300 py-1 px-2" 
