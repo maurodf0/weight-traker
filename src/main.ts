f446f3accc54791269999a84832aadd3cc11a916
Mauro <maurodefalco96@gmail.com>
2024-12-20 00:51:39 +0100

    no import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
- import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
- import NumberAnimation from "vue-number-animation";
+
 
 createApp(App).mount('#app')
