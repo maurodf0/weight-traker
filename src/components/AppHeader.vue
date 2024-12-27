2604a1d23f9e9a47f226e32a0e35a7d15a3e6b3e
Mauro <maurodefalco96@gmail.com>
2024-12-27 23:49:05 +0100

    tailwind changes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,16 +5,14 @@
 
 <template>
 
-    <header class="max-w-md mx-auto bg-white p-4 rounded-lg bg-slate-300">
+    <header class="max-w-md mx-auto my-3 bg-white p-4 rounded-lg bg-slate-300">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <div class="flex justify-between items-center border-b-2 border-gray-100 mb-6 pb-6">
-                <div class="flex items-center">
-                    <span class="ml-3 text-xl font-bold text-gray-900">Weight Traker</span>
-                </div>
+            <div class="flex justify-between items-center">
+                   <RouterLink to="/"><span class="ml-3 text-xl font-bold text-gray-900">Weight Traker</span> </RouterLink> 
+ 
                 <div class="flex items-center gap-x-4">
-                    <RouterLink to="/" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">Home</RouterLink>
                     <RouterLink to="/bmi" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">BMI</RouterLink>                   
-                    <RouterLink to="/tdee" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">Calculate TDEE</RouterLink>
+                    <RouterLink to="/tdee" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">TDEE</RouterLink>
                     <RouterLink to="/settings" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">Settings</RouterLink>
                 </div>
             </div>
