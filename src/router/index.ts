34e319e4035796896272121ea19e23619e4e636f
Mauro <maurodefalco96@gmail.com>
2024-12-27 23:50:55 +0100

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import SettingsView from '@/views/SettingsView.vue';
+import BMIView from '@/views/BMIView.vue';
+import TDEEView from '@/views/TDEEView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +12,24 @@ const router = createRouter({
       name: 'home',
       alias: ['/home', '/same'],
       component: HomeView
+    },
+    {
+         path: '/settings',
+         name: 'settings',
+         alias: ['/settings', '/settings'],
+         component: SettingsView
+    },
+    {
+      path: '/bmi',
+      name: 'bmi',
+      alias: ['/bmi', '/bmi'],
+      component: BMIView
+    },
+    {
+      path: '/tdee',
+      name: 'tdee',
+      alias: ['/tdee', '/tdee'],
+      component: TDEEView   
     },
 ]
 });
