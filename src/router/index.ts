3cd0db5668f8e75e4c0078d18feb6d8b20043c08
Mauro <maurodefalco96@gmail.com>
2024-12-27 23:54:57 +0100

    create pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 import SettingsView from '@/views/SettingsView.vue';
 import BMIView from '@/views/BMIView.vue';
 import TDEEView from '@/views/TDEEView.vue';
---
+++ src/views/TDEEView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div>
+        <h1>TDEE</h1>
+    </div>
+</template>
