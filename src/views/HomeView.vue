462673ef67f4c7ebd916b16e918fafa6896ce451
Mauro <maurodefalco96@gmail.com>
2024-12-28 00:33:27 +0100

    height in settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -136,37 +136,7 @@ const submitForm = (e: Event) => {
 const handleDelete = (date: Date) => {
   allWeights.value = allWeights.value.filter(w => w.date !== date)
 }
-const submitHeight = (newHeight: number) => {
-  height.value = newHeight
-}
 
-const heightinMeters = computed(() => {
-  return height.value / 100
-})
-
-
-// Computed property per calcolare il BMI
-const BMI = computed(() => {
-  if (height.value) {
-   return currentWeight.value / (heightinMeters.value * heightinMeters.value) 
-  }
-  return null
-})
-
-const BMIMessage = computed(() => {
-  if (BMI.value) {
-    if (BMI.value < 18.5) {
-      return 'You are Underweight'
-    } else if (BMI.value < 25) {
-      return 'You are Normal'
-    } else if (BMI.value < 30) {
-      return 'You are Overweight'
-    } else {
-      return 'You are Obese'
-    }
-  }
-  return null
-})
 
 </script>
 
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -5,6 +5,11 @@
     <button class="bg-slate-600 my-4 text-white py-2 px-4 rounded-lg" @click="toggleMode">
       Change Color Mode, current mode: {{ mode.value }}
     </button>
+
+    <h2>Height</h2>
+       <HeightInput class="mt-10" @heightHandle="submitHeight" />
+    <h4 class="text-2xl font-bold text-center text-gray-600">{{ BMIMessage }}</h4>
+
   </div>
 </template>
 
@@ -17,4 +22,36 @@ const toggleMode = () => {
   // Accediamo a mode.value per leggere e modificare la modalità
   mode.value = mode.value === 'dark' ? 'light' : 'dark'
 }
+
+const submitHeight = (newHeight: number) => {
+  height.value = newHeight
+}
+
+const heightinMeters = computed(() => {
+  return height.value / 100
+})
+
+
+// Computed property per calcolare il BMI
+const BMI = computed(() => {
+  if (height.value) {
+   return currentWeight.value / (heightinMeters.value * heightinMeters.value) 
+  }
+  return null
+})
+
+const BMIMessage = computed(() => {
+  if (BMI.value) {
+    if (BMI.value < 18.5) {
+      return 'You are Underweight'
+    } else if (BMI.value < 25) {
+      return 'You are Normal'
+    } else if (BMI.value < 30) {
+      return 'You are Overweight'
+    } else {
+      return 'You are Obese'
+    }
+  }
+  return null
+})
 </script>
