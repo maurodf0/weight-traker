4ba8bc744d7da3524cd72e8681f55dd977fa9040
Mauro <maurodefalco96@gmail.com>
2024-12-27 23:40:58 +0100

    install router in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import router from './router'
 import App from './App.vue'
 
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
