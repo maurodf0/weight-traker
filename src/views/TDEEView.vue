009793118bc3fb0b416b3ab9fc02e057829c4c0b
Mauro <maurodefalco96@gmail.com>
2025-01-03 00:36:43 +0100

    tdee calculation

--- src/views/TDEEView.vue
+++ src/views/TDEEView.vue
@@ -14,12 +14,13 @@ const { age } = useAge()
 
 const tdee = computed(() => {
   if (sex.value && activity.value && height.value) {
-    // Sex: MB = 66,4730 + (13,7516 x peso in kg) + (5,0033 x statura in cm) – (6,7550 x età in anni)
-    // Donne: MB = 655,0955 + (9,5634 x peso in kg) + (1,8496 x statura in cm) – (4,6756 x età in anni)
-    if (sex.value === 66.4730) {
-        return (sex.value + (9.5634 * height.value) + (1.8496 * height.value) - (4.6756 * age.value)).toFixed(2);
-    } else if (sex.value === 655.0955) {
-        return (sex.value + (13.7516 * height.value) + (5.0033 * height.value) - (6.7550 * age.value)).toFixed(2);
+    const sexAsNumber = Number(sex.value);
+    if (sexAsNumber === 66.4730) {
+        return (sexAsNumber + (9.5634 * height.value) + (1.8496 * height.value) - (4.6756 * age.value)).toFixed(2) + ' kcal';
+    } else if (sexAsNumber === 655.0955) {
+        return (sexAsNumber + (13.7516 * height.value) + (5.0033 * height.value) - (6.7550 * age.value)).toFixed(2) + ' kcal';
+    } else {
+        return ' Please enter your sex and height to calculate your TDEE';
     }
   }
 })
@@ -31,6 +32,6 @@ const tdee = computed(() => {
     <div>
         <h1>TDEE</h1>
         <p>Your TDEE is: {{ tdee }}</p>
-
+    
     </div>
 </template>
