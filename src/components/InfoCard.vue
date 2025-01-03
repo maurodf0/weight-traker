caeaf2124fb741de69ffaac42a2c1e38608969f5
Mauro <maurodefalco96@gmail.com>
2025-01-03 23:18:18 +0100

    suffix

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -9,6 +9,10 @@ defineProps({
     description: {
         type: String,
         required: true
+    },
+    suffix: {
+        type: String,
+        default: ''
     }
 })
 
@@ -17,7 +21,7 @@ defineProps({
 <template>
     <div class="flex flex-col gap-2 p-6 bg-gray-100 dark:bg-slate-700 rounded-lg">
         <h2 class="text-3xl font-semibold text-gray-800 dark:text-gray-100">
-            {{ info }}
+            {{ info }} / <span class="text-gray-500 dark:text-gray-400 text-sm"> {{ suffix }} </span>
         </h2>
         <p class="text-gray-600 dark:text-gray-300">
             {{ description }}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,26 +3,22 @@ import InfoCard from '@/components/InfoCard.vue';
 import useTracker from '@/composables/useTracker';
 
 const { weight, tdee, bmi } = useTracker();
-
-
 </script>
 
 <template>
-  <div class="max-w-3xl mx-auto my-8 p-6 bg-white dark:bg-gray-800 rounded-lg">
+  <div v-if="!weight && !bmi && !tdee">Loading...</div>
+  <div v-else class="max-w-3xl mx-auto my-8 p-6 bg-white dark:bg-gray-800 rounded-lg">
     <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-4">Weight Tracker</h1>
-
     <div class="flex flex-col gap-4">
-        <RouterLink to="/weight">
-             <InfoCard :info="weight" description="Weight"/>
-        </RouterLink>
-    
-          <RouterLink to="/bmi">
-             <InfoCard :info="bmi" description="BMI"/>
-        </RouterLink>
-
-           <RouterLink to="/tdee">
-             <InfoCard :info="tdee" description="TDEE"/>
-        </RouterLink>
+      <RouterLink to="/weight">
+        <InfoCard :info="weight" description="Weight" />
+      </RouterLink>
+      <RouterLink to="/bmi">
+        <InfoCard :info="bmi" description="BMI" />
+      </RouterLink>
+      <RouterLink to="/tdee">
+        <InfoCard :info="tdee" description="TDEE" />
+      </RouterLink>
     </div>
   </div>
 </template> 
