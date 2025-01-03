25746c1a709930a69ed1591fce51efc5a1c05847
Mauro <maurodefalco96@gmail.com>
2025-01-03 23:21:55 +0100

    recap (not working yet)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,10 @@ const { weight, tdee, bmi } = useTracker();
 
 <template>
   <div class="max-w-3xl mx-auto my-8 p-6 bg-white dark:bg-gray-800 rounded-lg">
-    <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-4">Weight Tracker</h1>
+    <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-4">Your Recap Tracked</h1>
     <div class="flex flex-col gap-4">
       <RouterLink to="/weight">
-        <InfoCard :info="weight" description="Weight" suffix="/kg" />
+        <InfoCard :info="weight" description="Weight" suffix="kg" />
       </RouterLink>
       <RouterLink to="/bmi">
         <InfoCard :info="bmi" description="BMI"   />
