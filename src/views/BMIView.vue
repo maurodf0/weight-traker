df7851c14bd712c8a27066067d2e89a07dd79551
Mauro <maurodefalco96@gmail.com>
2025-01-08 23:55:13 +0100

    config for chart

--- src/views/BMIView.vue
+++ src/views/BMIView.vue
@@ -14,24 +14,20 @@ const { setBMI } = useTracker();
         type VueUiGaugeConfig 
     } from "vue-data-ui";
     import "vue-data-ui/style.css";
+  
+    
+    
 
-//     under 18.5 – This is described as underweight.
-// between 18.5 and 24.9 – This is described as the 'healthy range'.
-// between 25 and 29.9 – This is described as overweight.
-// between 30 and 39.9 – This is described as obesity.
-// 40 or over – This is described as severe obesity.
+    const config = ref({"responsive":false,"theme":"","customPalette":[],"style":{"fontFamily":"inherit","chart":{"backgroundColor":"#ffffff00","color":"#1A1A1Aff","animation":{"use":true,"speed":"1","acceleration":1},"layout":{"radiusRatio":1,"track":{"size":1,"useGradient":false,"gradientIntensity":"13"},"markers":{"show":true,"color":"#1A1A1Aff","bold":true,"fontSizeRatio":1,"offsetY":0,"roundingValue":0,"formatter":null,"prefix":"","suffix":""},"segmentSeparators":{"show":false,"offsetOut":0,"offsetIn":0,"stroke":"#1A1A1Aff","strokeWidth":1},"segmentNames":{"show":true,"curved":true,"offsetRatio":"1.12","fontSize":16,"useSerieColor":true,"color":"#1A1A1Aff","bold":false},"pointer":{"type":"rounded","size":1,"stroke":"#1a1a1a00","strokeWidth":19,"useRatingColor":true,"color":"#CCCCCCff","circle":{"radius":19,"stroke":"#1a1a1a00","strokeWidth":12,"color":"#FFFFFFff"}}},"legend":{"show":true,"fontSize":48,"prefix":"","suffix":"","roundingValue":1,"showPlusSymbol":true,"useRatingColor":true,"color":"#1A1A1Aff","formatter":null},"title":{"text":"BMI","color":"#1A1A1Aff","fontSize":20,"bold":true,"textAlign":"center","paddingLeft":0,"paddingRight":0,"subtitle":{"color":"#A1A1A1ff","text":"","fontSize":16,"bold":false}}}},"userOptions":{"show":false,"showOnChartHover":false,"keepStateOnChartLeave":true,"position":"right","buttons":{"tooltip":false,"pdf":false,"csv":false,"img":false,"table":false,"labels":false,"fullscreen":false,"sort":false,"stack":false,"animation":false,"annotator":false},"buttonTitles":{"open":"Open options","close":"Close options","pdf":"Download PDF","img":"Download PNG","fullscreen":"Toggle fullscreen","annotator":"Toggle annotator"}},"translations":{"base":"Base"}})
     
-    
-    const config = ref<VueUiGaugeConfig>();
 
     const dataset = {
-    base: 200000,
-    value: bmi.value,
+    value: 25,
     series: [
         {
             from: 0,
             to: 18,
-            color: "#CC6600",
+            color: "#F87907",
             name: 'underweight'
         },
         {
@@ -43,7 +39,7 @@ const { setBMI } = useTracker();
         {
             from: 25,
             to: 29.9,
-            color: "#CC6600",
+            color: "#F87907",
             name: 'overweight'
         },
         {
