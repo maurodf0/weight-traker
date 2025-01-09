51b5307129a011dead6073bc7fbf76211ab50230
Mauro <maurodefalco96@gmail.com>
2025-01-09 01:06:21 +0100

    minimal PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'Weight Tracker', 
+        short_name: 'Weight Tracker',
+        description: 'A simple weight tracker app, built with Vue.js and Tailwind CSS. Track your weight, TDEE, and BMI.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
