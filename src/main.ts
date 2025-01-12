468e5b594d0462f8cd35ad5e1acfe7b29422899f
Mauro <maurodefalco96@gmail.com>
2025-01-12 19:01:14 +0100

    remove Meta

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
-import Meta from 'vue-meta';
+
 
 
 createApp(App)
   .use(router)
-  .use(Meta)
   .mount('#app')
