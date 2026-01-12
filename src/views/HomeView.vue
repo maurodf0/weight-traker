c570db340937f2088c76810459ca232e0e39e9ba
Mauro <maurodefalco96@gmail.com>
2026-01-12 16:50:29 +0100

    refactor(weight): replace useTracker with useWeight composable and update weight display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import InfoCard from '@/components/InfoCard.vue';
-import useTracker from '@/composables/useTracker';
+import useWeight from '@/composables/useWeight';
 import useCalculations from '@/composables/useCalculations';
 
-const { weight } = useTracker();
+const { currentWeight } = useWeight();
 
 const { tdee, bmi } = useCalculations();
 </script>
@@ -13,7 +13,7 @@ const { tdee, bmi } = useCalculations();
     <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-4">Your Recap Tracked</h1>
     <div class="flex flex-col gap-4">
       <RouterLink to="/weight">
-        <InfoCard :info="weight" description="Weight" suffix="kg" />
+        <InfoCard :info="currentWeight.toString()" description="Weight" suffix="kg" />
       </RouterLink>
       <RouterLink to="/bmi">
         <InfoCard :info="bmi" description="BMI"   />
